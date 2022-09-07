bc7b74bf822eb189c41037ae7f2eaf7e6e378eed
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-07 16:23:40 +0900

    fix(memo) : password 최소 길이 6자로 수정

--- src/memo/dto/createMemoRequest.dto.ts
+++ src/memo/dto/createMemoRequest.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateMemoRequestDto {
   @IsNotEmpty()
@@ -13,6 +13,6 @@ export class CreateMemoRequestDto {
 
   @IsNotEmpty()
   @IsString()
-  @MaxLength(100)
+  @MinLength(6)
   readonly password: string;
 }
