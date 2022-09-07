048a2449b978a11880cf69495a0074915ca05df9
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-07 21:01:09 +0900

    test(memo) : memo 등록 api 테스트 코드 작성

--- src/memo/test/memo.service.spec.ts
+++ src/memo/test/memo.service.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateMemoRequestDto } from '../dto/createMemoRequest.dto';
+import { MemoResponseDto } from '../dto/memoResponse.dto';
+import { Memo } from '../entity/memo.entity';
 import { MemoService } from '../memo.service';
+import * as bcrypt from 'bcryptjs';
 
 describe('MemoService', () => {
-  let service: MemoService;
+  let memoService: MemoService;
+
+  jest.mock('bcryptjs', () => {
+    return {
+      genSalt: jest.fn(() => 'hash'),
+      hash: jest.fn(() => 'hashPassword'),
+    };
+  });
+
+  const mockMemoRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MemoService],
+      providers: [
+        MemoService,
+        {
+          provide: getRepositoryToken(Memo),
+          useValue: mockMemoRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<MemoService>(MemoService);
+    memoService = module.get<MemoService>(MemoService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(memoService).toBeDefined();
+  });
+
+  describe('createMemo', () => {
+    it('메모 작성', async () => {
+      //given
+      const input: CreateMemoRequestDto = {
+        title: '제목',
+        content: '내용',
+        password: '비밀번호12',
+      };
+
+      const createMemo: Memo = {
+        id: 1,
+        title: '제목',
+        content: '내용',
+        password: 'hash비밀번호12',
+        createAt: undefined,
+        updateAt: undefined,
+        deleteAt: undefined,
+        toResponse: function (): MemoResponseDto {
+          return {
+            memoId: this.id,
+            title: this.title,
+            content: this.content,
+          };
+        },
+      };
+
+      mockMemoRepository.create.mockImplementation(
+        (input: CreateMemoRequestDto) => createMemo,
+      );
+      mockMemoRepository.save.mockImplementation((memo) => createMemo);
+
+      //when
+      const result = await memoService.createMemo(input);
+
+      //then
+      expect(result).toEqual(createMemo.toResponse());
+    });
   });
 });
